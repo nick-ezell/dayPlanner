a7ebc4e815fc728884e89373fbdc3be2d64e01f5
Nick Ezell <alexsandratrammell@Nicks-MacBook-Pro.local>
2020-01-26 00:53:12 -0600

    Added function to set dynamic time and centered input text in textareas.

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 $(document).ready(function (){
+    var datetime = null,
+        date = null;
 
+var update = function () {
+    date = moment(new Date())
+    datetime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
+};
+
+$(document).ready(function(){
+    datetime = $('header')
+    update();
+    setInterval(update, 1000);
+});
 });
